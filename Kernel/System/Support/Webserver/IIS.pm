1e098e6c066832780faf20fc8efd41d656b2c769
Michiel Beijen <michiel.beijen@otrs.com>
2013-02-04 07:08:41 +0000

    Added IIS tests to show web server version and variables.

--- Kernel/System/Support/Webserver/IIS.pm
+++ Kernel/System/Support/Webserver/IIS.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/Support/Webserver/IIS.pm - all required system information
-# Copyright (C) 2001-2012 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2013 OTRS AG, http://otrs.org/
 # --
-# $Id: IIS.pm,v 1.10 2012-09-05 04:29:56 cg Exp $
+# $Id: IIS.pm,v 1.11 2013-02-04 07:08:41 mb Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 
 use vars qw(@ISA $VERSION);
-$VERSION = qw($Revision: 1.10 $) [1];
+$VERSION = qw($Revision: 1.11 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -75,7 +75,6 @@ sub _PerlExCheck {
 
     my $Data = {};
 
-    # check if Apache::DBI is loaded
     my $Check   = '';
     my $Message = '';
     if ( $ENV{'GATEWAY_INTERFACE'} && $ENV{'GATEWAY_INTERFACE'} =~ /^CGI-PerlEx/i ) {
@@ -93,6 +92,58 @@ sub _PerlExCheck {
         Description => $Self->{LanguageObject}->Get('Check if PerlEx is used.'),
         Comment     => $Message,
         Check       => $Check,
+    };
+    return $Data;
+}
+
+sub _IISVersionCheck {
+    my ( $Self, %Param ) = @_;
+
+    my $Data    = {};
+    my $Check   = '';
+    my $Message = '';
+    if ( $ENV{SERVER_SOFTWARE} ) {
+        $Check   = 'OK';
+        $Message = $Self->{LanguageObject}->Get('You are running') . " $ENV{SERVER_SOFTWARE}.";
+    }
+    else {
+        $Check   = 'Failed';
+        $Message = $Self->{LanguageObject}->Get('Could not determine IIS version.');
+    }
+    $Data = {
+        Name        => $Self->{LanguageObject}->Get('IIS Version'),
+        Description => $Self->{LanguageObject}->Get('Display web server version.'),
+        Comment     => $Message,
+        Check       => $Check,
+    };
+    return $Data;
+}
+
+sub _IISEnvironmentCheck {
+    my ( $Self, %Param ) = @_;
+
+    my %Environment = %ENV;
+
+    for my $NotNeededString (
+        qw( HTTP_REFERER         HTTP_CACHE_CONTROL   HTTP_COOKIE    HTTP_USER_AGENT
+        HTTP_ACCEPT_LANGUAGE HTTP_ACCEPT_ENCODING HTTP_ACCEPT
+        QUERY_STRING         REQUEST_METHOD       REQUEST_URI    SCRIPT_NAME
+        ALLUSERSPROFILE      APPDATA              LOCALAPPDATA   COMMONPROGRAMFILES
+        PROGRAMDATA          PROGRAMFILES         PSMODULEPATH   PUBLIC
+        SYSTEMDRIVE          SYSTEMROOT           TEMP           WINDIR
+        USERPROFILE )
+        )
+    {
+        delete $Environment{$NotNeededString};
+    }
+
+    my $Data = {
+        Name        => $Self->{LanguageObject}->Get('Server Environment'),
+        Description => $Self->{LanguageObject}->Get('Display environment variables'),
+        Comment     => $Self->{LanguageObject}->Get('General information about your system.'),
+        Check       => 'OK',
+        BlockStyle  => 'TableDataSimple',
+        TableInfo   => \%Environment,
     };
     return $Data;
 }
